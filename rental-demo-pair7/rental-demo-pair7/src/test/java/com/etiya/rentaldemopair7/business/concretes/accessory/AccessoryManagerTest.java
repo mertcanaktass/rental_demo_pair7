4a9d59e72d076f073ffa87e359d3932c0571c142
mert.aktas <mertcanaktass97@gmail.com>
2023-04-30 21:31:24 +0300

    AccessoryManagerTest yapısı ekledi.

---
+++ rental-demo-pair7/rental-demo-pair7/src/test/java/com/etiya/rentaldemopair7/business/concretes/accessory/AccessoryManagerTest.java
@@ -0,0 +1,22 @@
+package com.etiya.rentaldemopair7.business.concretes.accessory;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccessoryManagerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+}
